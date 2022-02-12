ede978a168d384492a10d4e4f1d8a31b35ea7ee4
furikake <70467301+furikake-aoi@users.noreply.github.com>
2022-02-12 17:59:13 +0900

    test

--- cenge-lang.js
+++ cenge-lang.js
@@ -1,12 +1,13 @@
-const selected = document.getElementById("clang");
-
-selected.addEventListener('change', function(){
-  var lang = selected.value;
+function langchange(event){
+  const selected = document.getElementById('clang');
+  const lan = selected.selectedIndex;
+  const lang = selected.options[lan].value;
+  console.log(lang);
   var npath = location.pathname;
   if(npath === "/index.html"){
     location.href = "https://furikake.f5.si/"+lang+npath;
   } else{
-    var npath = npath.slice( 3 );
-    location.href = "https://furikake.f5.si/"+lang+npath;
+    var ntpath = npath.slice( 3 );
+    location.href = "https://furikake.f5.si/"+lang+ntpath;
   }
-});
+};
