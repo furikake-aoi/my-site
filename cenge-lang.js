b2a5be98b109ff4ddfd837928613d797ab9c2e62
furikake <70467301+furikake-aoi@users.noreply.github.com>
2022-02-12 19:27:32 +0900

    test

--- cenge-lang.js
+++ cenge-lang.js
@@ -3,7 +3,8 @@ function langchange(event){
   const lan = selected.selectedIndex;
   const lang = selected.options[lan].value;
   var npath = location.pathname;
-  if(npath === "/index.html"){
+  console.log(npath);
+  if(npath === "" || npath.startsWith('/index')){
     location.href = "https://furikake.f5.si/"+lang+npath;
   } else{
     var ntpath = npath.slice( 3 );
