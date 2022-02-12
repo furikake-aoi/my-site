0de3e9f70547c064a2022af631e959812ca0a5a8
furikake <70467301+furikake-aoi@users.noreply.github.com>
2022-02-12 23:04:07 +0900

    Update cenge-lang.js

--- cenge-lang.js
+++ cenge-lang.js
@@ -3,15 +3,11 @@ function langchange(event){
   const lan = selected.selectedIndex;
   const lang = selected.options[lan].value;
   var npath = location.pathname;
-  console.log(npath);
-  console.log(npath.startsWith('/en' || '/zht' || '/hi' || '/kt' || '/ja'))
   if(npath.length === 1 || npath.startsWith('/index.html')){
-    console.log('https://furikake.f5.si/'+lang+'/index.html')
-    //location.href = "https://furikake.f5.si/"+lang+"/index.html";
+    location.href = "https://furikake.f5.si/"+lang+"/index.html";
   } else{
     if(npath.startsWith('/zht')){var ntpath = str.slice( 4 )}
-    else {var ntpath = str.slice( 3 )}
-    console.log('https://furikake.f5.si/'+lang+'ntpath')
-    //location.href = "https://furikake.f5.si/"+lang+ntpath;
+    else {var ntpath = npath.slice( 3 )}
+    location.href = "https://furikake.f5.si/"+lang+ntpath;
   }
 };
