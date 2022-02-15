b671051d42cabf7514b698bb037c0efb2fd97c48
furikake <70467301+furikake-aoi@users.noreply.github.com>
2022-02-15 18:10:33 +0900

    test

--- cenge-lang.js
+++ cenge-lang.js
@@ -6,7 +6,7 @@ function langchange(event){
   if(npath.length === 1 || npath.startsWith('/index.html')){
     location.href = "https://furikake.f5.si/"+lang+"/index.html";
   } else{
-    if(npath.startsWith('/zht')){var ntpath = str.slice( 4 )}
+    if(npath.startsWith('/zht')){var ntpath = npath.slice( 4 )}
     else {var ntpath = npath.slice( 3 )}
     location.href = "https://furikake.f5.si/"+lang+ntpath;
   }
